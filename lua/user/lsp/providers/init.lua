ffbc69287251ba42bbf5176377285670be6de3a2
3Xpl0it3r <shouc.wang@hotmail.com>
2023-02-21 19:49:39 +0800

    rename

--- lua/user/lsp/providers/init.lua
+++ lua/user/lsp/providers/init.lua
@@ -1,9 +1,9 @@
 local M = {}
 
 
-for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath("config") .. "/lua/user/lsp/config", [[v:val =~ '\.lua$']])) do
+for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath("config") .. "/lua/user/lsp/providers", [[v:val =~ '\.lua$']])) do
 	if not string.find(string.lower(file), "init.lua") then
-		M = vim.tbl_deep_extend("force", M, require("user.lsp.config." .. file:gsub("%.lua$", "")))
+		M = vim.tbl_deep_extend("force", M, require("user.lsp.providers." .. file:gsub("%.lua$", "")))
 	end
 end
 
