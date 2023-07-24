cd98e529d9b9ceb43a5a0b755a602738fd730460
l0calh0st <shouc.wang@hotmail.com>
2023-07-24 18:23:32 +0800

    fix(neo-tree): add config

--- lua/plugin.lua
+++ lua/plugin.lua
@@ -74,7 +74,7 @@ local plugins = {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		config = function()
-		require("plugins.nvim-treesitter").setup()
+			require("plugins.nvim-treesitter").setup()
 		end,
 	},
 	------------------------------------------------
@@ -115,6 +115,7 @@ local plugins = {
 			"nvim-tree/nvim-web-devicons",
 			"MunifTanjim/nui.nvim",
 		},
+		opts = require("plugins.neo-tree").new_options(),
 	},
 	{
 		"JoosepAlviste/nvim-ts-context-commentstring",
