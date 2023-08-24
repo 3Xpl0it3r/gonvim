da747e6bf47587b8456c5ca85f2bd8765fb74b56
l0calh0st <shouc.wang@hotmail.com>
2023-08-24 17:33:37 +0800

    chore(dap): add keymap for clean all breadpoint

--- lua/user/keybinds/dap.lua
+++ lua/user/keybinds/dap.lua
@@ -2,30 +2,32 @@ local M = {}
 
 M.key_binds = function(dap, dap_ui)
 	-- ---------------------dap keymap configuration --------------------------
-    
-    -- system manage reference
-	vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "DAP: Continue" })
-	vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "DAP: Toggle breackpoint" })
-	vim.keymap.set("n", "<leader>dB", function()
-		require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-	end, { desc = "DAP: Set breakpoint" })
 
+	-- system manage reference
+	vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "DAP: Continue" })
 
 	vim.keymap.set("n", "<leader>dR", dap.restart, { desc = "DAP: Restart process" })
 	vim.keymap.set("n", "<leader>de", dap.set_exception_breakpoints, { desc = "DAP: Set exception breakpoints" })
-    -- terminalte dap 
+	-- terminalte dap
 	vim.keymap.set("n", "<leader>dT", dap.terminate, { desc = "DAP: Dap Terminated" })
 
+	-- breakpioints
+	vim.keymap.set("n", "<leader>dB", function()
+		require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+	end, { desc = "DAP: Set breakpoint" })
+	vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "DAP: Toggle breackpoint" })
+	vim.keymap.set("n", "<leader>dl", dap.list_breakpoints, { desc = "DAP: List Breakpoints" })
+	vim.keymap.set("n", "<leader>dC", dap.clear_breakpoints, { desc = "DAP: Clear All Breakpoints" })
 
-    -- debug reference  
-    -- running programming
+	-- debug reference
+	-- running programming
 	vim.keymap.set("n", "<leader>dn", dap.step_over, { desc = "DAP: Step over to next cursor line" })
 	vim.keymap.set("n", "<leader>ds", dap.step_into, { desc = "DAP: Single step through program" })
 	vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "DAP: Step out of the current function" })
 
 	vim.keymap.set("n", "<leader>dj", dap.run_to_cursor, { desc = "DAP: Run to cursor" }) -- for jump
 
-    -- console reference
+	-- console reference
 	vim.keymap.set("n", "<leader>dr", dap.repl.toggle, { desc = "DAP: Toggle REPL" })
 
 	vim.keymap.set("n", "<leader>dx", require("dapui").eval, { desc = "DAP-UI: Eval" })
