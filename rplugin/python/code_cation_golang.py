bbfe14869d4d796e4806a7c185b11a33a2bad71e
l0calh0st <shouc.wang@hotmail.com>
2023-08-29 10:56:17 +0800

    fix(rplugin): create dir if dir is not existed, when call k8s codeaction

--- rplugin/python/code_cation_golang.py
+++ rplugin/python/code_cation_golang.py
@@ -7,9 +7,12 @@ import os
 from pathlib import Path
 
 ALL_MODULES = "ALL_MODULES"
+
+
 def cache_k8s_module(cache_file):
     with open(cache_file, "r") as fp:
-        ret = [_.lstrip("./staging/src/") for _ in fp.read().split() if "./staging/src/" in _]
+        ret = [_.lstrip("./staging/src/")
+               for _ in fp.read().split() if "./staging/src/" in _]
         ret.append(ALL_MODULES)
         return ret
 
@@ -21,27 +24,34 @@ def cache_k8s_mod_info(cache_file):
 
 @pynvim.plugin
 class CodeActionGolangK8s(object):
-    def __init__(self, nvim:Nvim):
+    def __init__(self, nvim: Nvim):
         self.nvim = nvim
         self.mod_api = "https://raw.githubusercontent.com/kubernetes/kubernetes/v{}/go.mod"
-        self.cache_dir = os.path.join(Path.home(), ".config/nvim/rplugin/python/cache/k8s")
+        self.cache_dir = os.path.join(
+            Path.home(), ".config/nvim/rplugin/python/cache/k8s")
 
     @pynvim.function(name="K8sAllModules", sync=True)
     def gather_all_k8s_modules(self, k8s_version):
         if len(k8s_version) != 1:
             return None
         k8s_version = k8s_version[0]
-        cache_file = os.path.join(self.cache_dir, "{}/go.mod".format(k8s_version))
+        cache_file = os.path.join(
+            self.cache_dir, "{}/go.mod".format(k8s_version))
+        #read mod info from cache file directory,if relatived cache file existed
         if os.path.exists(cache_file):
             return cache_k8s_module(cache_file=os.path.join(self.cache_dir, "{}/go.mod".format(k8s_version)))
         try:
-            response = urlopen(self.mod_api.format(k8s_version), timeout=20).read().decode("utf-8")
+            response = urlopen(self.mod_api.format(
+                k8s_version), timeout=20).read().decode("utf-8")
         except Exception as error:
             return None
-        with open(cache_file, "wb") as fp:
-            fp.write(response)
+        if not os.path.exists(os.path.join(self.cache_dir, "{}".format(k8s_version))):
+            os.mkdir(os.path.join(self.cache_dir, "{}".format(k8s_version)))
+        with open(cache_file, "wb") as writer:
+            writer.write(response)
         modules = set(ALL_MODULES)
-        [modules.add(_.strip("./staging/src/")) for _ in response.split() if "./staging/src/" in _]
+        [modules.add(_.strip("./staging/src/"))
+         for _ in response.split() if "./staging/src/" in _]
         return modules
 
     @pynvim.command("CacheK8sClean")
@@ -72,12 +82,14 @@ class CodeActionGolangK8s(object):
         return "OK"
 
     def go_mod_add_pkg(self, mod_name, version):
-        cache_file = os.path.join(self.cache_dir, "{}/{}.json".format(version, mod_name.split("/")[-1]))
+        cache_file = os.path.join(
+            self.cache_dir, "{}/{}.json".format(version, mod_name.split("/")[-1]))
         if os.path.exists(cache_file):
             json_data = cache_k8s_mod_info(cache_file)
         else:
             try:
-                cmd = 'go mod download -json "{}@kubernetes-{}"'.format(mod_name, version)
+                cmd = 'go mod download -json "{}@kubernetes-{}"'.format(
+                    mod_name, version)
                 code, resp = subprocess.getstatusoutput(cmd)
                 if code:
                     return None
@@ -89,16 +101,16 @@ class CodeActionGolangK8s(object):
             # with open(cache_file,"wb") as fp:
             #     json.dump(json_data, fp)
         version = json_data.get("Version")
-        cmd = 'go mod edit "-replace={MOD_NAME}={MOD_NAME}@{VERSION}"'.format(MOD_NAME=mod_name, VERSION=version)
+        cmd = 'go mod edit "-replace={MOD_NAME}={MOD_NAME}@{VERSION}"'.format(
+            MOD_NAME=mod_name, VERSION=version)
         code, _ = subprocess.getstatusoutput(cmd)
         if code:
             return None
         return "Ok"
 
 
-
 if __name__ == '__main__':
     k8s = CodeActionGolangK8s()
     all_modes = k8s.gather_all_k8s_modules(["1.16.0"])
-    a = k8s.k8s_mod_download([ALL_MODULES,"1.16.0"])
+    a = k8s.k8s_mod_download([ALL_MODULES, "1.16.0"])
     print(a)
