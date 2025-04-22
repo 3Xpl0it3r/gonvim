23bc132b334dea961d917c4de21beffd64724879
3Xpl0it3r <shouc.wang@hotmail.com>
2025-04-22 21:08:31 +0800

    fix(lsp): vim.lsp.handlers.hover deprecated since nvim 0.11

--- lua/plugins/lsp/handler.lua
+++ lua/plugins/lsp/handler.lua
@@ -16,40 +16,6 @@ vim.api.nvim_create_autocmd("CursorHold", {
         vim.diagnostic.open_float(nil, opts)
     end,
 })
-local function goto_definition(split_cmd)
-    local util = vim.lsp.util
-    local log = require("vim.lsp.log")
-    local api = vim.api
-
-    -- note, this handler style is for neovim 0.5.1/0.6, if on 0.5, call with function(_, method, result)
-    local handler = function(_, result, ctx)
-        if result == nil or vim.tbl_isempty(result) then
-            local _ = log.info() and log.info(ctx.method, "No location found")
-            return nil
-        end
-
-        if split_cmd then
-            vim.cmd(split_cmd)
-        end
-
-        if vim.tbl_islist(result) then
-            util.jump_to_location(result[1])
-
-            if #result > 1 then
-                -- util.set_qflist(util.locations_to_items(result))
-                -- vim.fn.setqflist(util.locations_to_items(result, "utf-8"))
-                vim.fn.setloclist(0, util.locations_to_items(result, "utf-8"), " ")
-                api.nvim_command("copen")
-                api.nvim_command("wincmd p")
-                vim.api.nvim_command("cclose") -- for disable qflist
-            end
-        else
-            util.jump_to_location(result)
-        end
-    end
-
-    return handler
-end
 
 local function definition_split()
     vim.lsp.buf.definition({
@@ -100,18 +66,12 @@ M.setup = function()
         },
     }
     vim.diagnostic.config(config)
-    vim.lsp.handlers["textDocument/definition"] = goto_definition("vsplit")
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-    vim.lsp.handlers["textDocument/signatureHelp"] =
-        vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded", focusable = false, relative = "cursor" })
-
     vim.lsp.handlers["$/progress"] = require("utils.notify").lsp_status_update
 end
 
 local function lsp_keymaps(bufnr)
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-    -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
     vim.keymap.set("n", "gd", definition_split, { desc = "Goto Definition (split)" })
     vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
     vim.keymap.set("n", "gi", "<cmd>lua require('telescope.builtin').lsp_implementations()<CR>", { noremap = true })
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,34 +1,43 @@
 local status_ok, lspconfig = pcall(require, "lspconfig")
 if not status_ok then
-	require("utils.notify").notify("Plugin lspconfig is not existed", "error", "Plugin")
-	return
+    require("utils.notify").notify("Plugin lspconfig is not existed", "error", "Plugin")
+    return
 end
 
 local M = {}
 
-local function config_lspconfig(handler)
-	local opts = {
-		on_attach = handler.on_attach(),
-		capabilities = handler.capabilities(),
-	}
 
-	-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-	for server_name, server_config in pairs(require("user.lsp")) do
-		local opts_clone = opts
-		if server_config.server == "clangd" then
-			opts_clone.capabilities.offsetEncoding = { "utf-16" }
-		end
-		opts_clone = vim.tbl_deep_extend("force", server_config, opts)
-		lspconfig[server_name].setup(opts_clone)
-	end
+
+local function config_lspconfig(handler)
+    local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+    function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+        opts = opts or {}
+        opts.border = "rounded"
+        return orig_util_open_floating_preview(contents, syntax, opts, ...)
+    end
+
+    local opts = {
+        on_attach = handler.on_attach(),
+        capabilities = handler.capabilities(),
+    }
+
+    -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+    for server_name, server_config in pairs(require("user.lsp")) do
+        local opts_clone = opts
+        if server_config.server == "clangd" then
+            opts_clone.capabilities.offsetEncoding = { "utf-16" }
+        end
+        opts_clone = vim.tbl_deep_extend("force", server_config, opts)
+        lspconfig[server_name].setup(opts_clone)
+    end
 
     require("lspconfig.ui.windows").default_options.border = "rounded"
 end
 
 function M.setup()
-	local handler = require("plugins/lsp/handler")
-	handler.setup()
-	config_lspconfig(handler)
+    local handler = require("plugins/lsp/handler")
+    handler.setup()
+    config_lspconfig(handler)
 end
 
 return M
